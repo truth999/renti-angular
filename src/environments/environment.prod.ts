7b54c3af4b8641f866c315f5dc62650739e397f3
truth999 <truthdev999@gmail.com>
2019-04-18 02:55:21 +1000

    fixed some configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiBase: 'http://renti-dev.renti.hu:8080/api/v1/',
+  uploadApiBase: 'http://renti-dev.renti.hu:8080/api/v1/upload/',
+  uploadBase: 'http://renti-dev.renti.hu:8080/public/uploads/'
 };
