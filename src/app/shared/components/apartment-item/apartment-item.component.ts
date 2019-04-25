ba9ad82bd06fa77e424df74a683d168896501fa2
truth999 <truthdev999@gmail.com>
2019-04-26 02:11:51 +1000

    Updated

---
+++ src/app/shared/components/apartment-item/apartment-item.component.ts
@@ -0,0 +1,35 @@
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-apartment-item',
+  templateUrl: './apartment-item.component.html',
+  styleUrls: ['./apartment-item.component.scss']
+})
+export class ApartmentItemComponent implements OnInit {
+  @Input() imgUrl: string;
+  @Input() name: string;
+  @Input() explain: string;
+  @Input() mo: number;
+  @Input() square: number;
+  @Input() rooms: number;
+  @Input() data: number;
+  @Input() topRated: boolean;
+  @ViewChild('explain') explainParagraph: ElementRef;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.ellipsizeTextBox();
+  }
+
+  ellipsizeTextBox() {
+    const el = this.explainParagraph.nativeElement;
+    const wordArray = el.textContent.split(' ');
+
+    while (el.scrollHeight > el.offsetHeight) {
+      wordArray.pop();
+      el.textContent = wordArray.join(' ') + '...';
+    }
+  }
+
+}
