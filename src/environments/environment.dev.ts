abd46bb2f470bc3e462cd86eb848f824223cba34
truth999 <truthdev999@gmail.com>
2019-05-29 23:23:28 +1000

    Updated

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   apiBase: 'http://localhost:8080/api/v1/',
-  uploadApiBase: 'http://renti-dev.renti.hu:8080/api/v1/upload/',
-  uploadBase: 'http://renti-dev.renti.hu:8080/public/uploads/'
+  uploadApiBase: 'https://renti-dev.renti.hu/api/v1/upload/',
+  uploadBase: 'https://renti-dev.renti.hu/api/public/uploads/'
 };
